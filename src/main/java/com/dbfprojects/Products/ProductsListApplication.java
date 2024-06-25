29efcad825d13b8a853860e38db558e7be9f4b02
duartebfilho <duartebfilho@yahoo.com>
2024-06-25 11:57:02 +0200

    Products Project created

---
+++ src/main/java/com/dbfprojects/Products/ProductsListApplication.java
@@ -0,0 +1,13 @@
+package com.dbfprojects.Products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsListApplication.class, args);
+	}
+
+}
